e1734b22b2fc38d15b7ecf1ac6523966ec1b1c35
Léo RASPAUD <leo.raspaud@gmail.com>
2019-02-09 16:40:13 +1100

    Update APP to use react hooks

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,11 +5,29 @@ const isAuthenticated = () => window.localStorage.getItem(constants.LOCAL_STORAG
 const getUserId = () => JSON.parse(window.localStorage.getItem(constants.LOCAL_STORAGE_KEY)).id;
 const getUsername = () => JSON.parse(window.localStorage.getItem(constants.LOCAL_STORAGE_KEY)).username;
 const getUser = () => JSON.parse(window.localStorage.getItem(constants.LOCAL_STORAGE_KEY));
+const setUser = user => window.localStorage.setItem(constants.LOCAL_STORAGE_KEY, JSON.stringify(user));
+const isFromRedirect = () => window.location.href.indexOf('openid') > -1;
+
+const getProfileId = () => {
+  const parsed = window.location.href.split('&');
+  let profileId;
+  parsed.forEach(a => {
+    const split = a.split('openid.identity=');
+    if (split.length > 1) {
+      const identity = split[1];
+      profileId = identity.split('%2Fid%2F')[1];
+    }
+  });
+  return profileId;
+};
 
 export default {
+  setUser,
+  isFromRedirect,
   isAuthenticated,
   getUserId,
   removeUser,
   getUsername,
   getUser,
+  getProfileId,
 };
